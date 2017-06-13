2fa3048f49081f1e2047e1df9145a0f853f301ee
kwadrnt <kevinctse@gmail.com>
2017-06-13 16:37:59 -0700

    added map with marker data now working on customizing info window

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,13 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @trucks = Truck.all
+    @hash = Gmaps4rails.build_markers(@trucks) do |truck, marker|
+    marker.lat truck.latitude
+    marker.lng truck.longitude
+    marker.infowindow truck.name
+  end
+
   end
 
   def show
--- app/helpers/trucks_helper.rb
+++ app/helpers/trucks_helper.rb
@@ -1,2 +1,3 @@
 module TrucksHelper
+  
 end
